51798205af0cb0c767e04c82613ca667a56ecc3f
himubey <himubey@hotmail.com>
2023-08-18 16:23:33 +0530

    fix: share function

--- script.js
+++ script.js
@@ -13,21 +13,41 @@ const infmemes=()=>{
 
 infmemes();
 
-function share() {
-    if(navigator.share !==undefined){
-        navigator
-        .share({
-            title:"Infinity Memes",
-            text:"infinity Memes",
-            url:"url_data",
-        })
-        
-        .then(()=>console.log("Successful Share"))
-        .catch((error)=>console.log("Error",error))
-    } else {
-        console.log("Share API not available in this browser");
-    }
+const shareData = {
+         title:"Infinity Memes",
+         text:"infinity Memes",
+         url:"url_data",
 }
+
+const btn = document.querySelector("button")
+const sharePara = document.querySelector("share_res")
+
+// share must be triggered by "user activation"
+
+btn.addEventListener("click", async () => {
+    try{
+        await navigator.share(shareData);
+        sharePara.textContent = "MEME Share successfully";
+    } catch (err) {
+        resultPara.textContent = `Error: ${err}`;
+    }
+});
+
+// function share() {
+//     if(navigator.share !==undefined){
+//         navigator
+//         .share({
+//             title:"Infinity Memes",
+//             text:"infinity Memes",
+//             url:"url_data",
+//         })
+        
+//         .then(()=>console.log("Successful Share"))
+//         .catch((error)=>console.log("Error",error))
+//     } else {
+//         console.log("Share API not available in this browser");
+//     }
+// }
 
 function next() {
     infmemes();
