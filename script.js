646bd101f4d3dded1e7ba73221eb755f4e42d20e
himubey <himubey@hotmail.com>
2023-08-26 23:32:57 +0530

    fix: update codebase

--- script.js
+++ script.js
@@ -14,43 +14,43 @@ const infmemes=()=>{
 
 infmemes();
 
-const shareData = {
-         title:"Infinity Memes",
-         text:"infinity Memes",
-         url:url_data,
-};
+// const shareData = {
+//          title:"Infinity Memes",
+//          text:"infinity Memes",
+//          url:url_data,
+// };
 
-const btn = document.getElementById("btn");
-const sharePara = document.querySelector("share_res");
+// const btn = document.getElementById("btn");
+// const sharePara = document.querySelector("share_res");
 
-// share must be triggered by "user activation"
+// // share must be triggered by "user activation"
 
-btn.addEventListener("click", async () => {
-    try {
-        await navigator.share(shareData);
-        sharePara.textContent = "MEME Share successfully";
-    } catch (err) {
-        sharePara.textContent = `Error: ${err}`;
-    }
-});
+// btn.addEventListener("click", async () => {
+//     try {
+//         await navigator.share(shareData);
+//         sharePara.textContent = "MEME Share successfully";
+//     } catch (err) {
+//         sharePara.textContent = `Error: ${err}`;
+//     }
+// });
 
 console.log(shareData);
 
-// function share() {
-//     if(navigator.share !==undefined){
-//         navigator
-//         .share({
-//             title:"Infinity Memes",
-//             text:"infinity Memes",
-//             url:"url_data",
-//         })
+function share() {
+    if(navigator.share !=undefined){
+        navigator
+        .share({
+            title:"Infinity Memes",
+            text:"infinity Memes",
+            url:url_data,
+        })
         
-//         .then(()=>console.log("Successful Share"))
-//         .catch((error)=>console.log("Error",error))
-//     } else {
-//         console.log("Share API not available in this browser");
-//     }
-// }
+        .then(()=>console.log("Successful Share"))
+        .catch((error)=>console.log("Error",error))
+    } else {
+        console.log("Share API not available in this browser");
+    }
+}
 
 function next() {
     infmemes();
